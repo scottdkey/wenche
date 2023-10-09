15e1f45b9ab3ab4f7e4cdaba54f0caccf78d1079
Scott Key <scottdkey@gmail.com>
2023-10-08 18:44:27 -0600

    Paragraph update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,35 +59,37 @@
 <div class="main">
 	<h1 class="header">Wenche Donation</h1>
 	<img alt="Wenche Playing the flute" src={wenche} class="primaryPhoto" />
-	<p class="paragraphText">
-		Hvis du er her, er det en god sjangse for at Wenche har hjulpet deg på et tidspunkt, enten som
-		lærer, kollega eller medmenneske. Men nå er det Wenche som trenger vår hjelp. I perioden
-		mars/april 2023 ble hun svindlet av tre iranske menn, som påsto de trengte hjelp til å få sin
-		mor ut av Iran. Som alltid var Wenche godhjertet og ville hjelpe, selv om disse mennene til
-		tider opptredte truende. To av dem skaffet seg blandt annet adgang til leiligheten hennes, og
-		presset henne til å «hjelpe» dem. Historien deres var ren fabrikasjon, og hun mistet alle
-		sparepengene side – 45.000 Nok. Nå står Wenche i fare for å miste leiligheten sin, hvis hun ikke
-		klarer nedbetalingene. Derfor trenger vi DIN hjelp – uansett hvor mye eller lite du har mulighet
-		til å gi. Alle monner drar, og målet vårt er å overraske Wenche – derfor er det veldig viktig at
-		ingen nevner dette for henne. Dette er også grunnen til at vi ikke bruker etternavnet hennes
-		her. Vi har også satt målet til 48.000 pga at GoFundMe tar et gebyr per donasjon, slik at disse
-		gebyrene ikke vil «spise» seg inn i de 45.000 Wenche trenger.
-	</p>
-	<div>
-		<div>
-			<div class="container">
-				<div>
-					<button
-						type="button"
-						on:click={(e) => {
-							showCheckoutWindow(e);
-							window.open('https://square.link/u/6jSZn1WE?src=embed', '_blank');
-						}}
-						class="button">Donate</button
-					>
-				</div>
-			</div>
-		</div>
+	<div class="paragraphText">
+		<p>
+			Hvis du er her, er det en god sjangse for at Wenche har hjulpet deg på et tidspunkt, enten som
+			lærer, kollega eller medmenneske. Men nå er det Wenche som trenger vår hjelp. I perioden
+			mars/april 2023 ble hun svindlet av tre iranske menn, som påsto de trengte hjelp til å få sin
+			mor ut av Iran.
+		</p>
+		<p>
+			Som alltid var Wenche godhjertet og ville hjelpe, selv om disse mennene til tider opptredte
+			truende. To av dem skaffet seg blandt annet adgang til leiligheten hennes, og presset henne
+			til å «hjelpe» dem. Historien deres var ren fabrikasjon, og hun mistet alle sparepengene side
+			– 45.000 Nok. Nå står Wenche i fare for å miste leiligheten sin, hvis hun ikke klarer
+			nedbetalingene.
+		</p>
+		<p>
+			Derfor trenger vi DIN hjelp – uansett hvor mye eller lite du har mulighet til å gi. Alle
+			monner drar, og målet vårt er å overraske Wenche – derfor er det veldig viktig at ingen nevner
+			dette for henne. Dette er også grunnen til at vi ikke bruker etternavnet hennes her. Vi har
+			også satt målet til 48.000 pga at GoFundMe tar et gebyr per donasjon, slik at disse gebyrene
+			ikke vil «spise» seg inn i de 45.000 Wenche trenger.
+		</p>
+	</div>
+	<div class="container">
+		<button
+			type="button"
+			on:click={(e) => {
+				showCheckoutWindow(e);
+				window.open('https://square.link/u/6jSZn1WE?src=embed', '_blank');
+			}}
+			class="button">Donate</button
+		>
 	</div>
 </div>
 
@@ -115,38 +117,38 @@
 		margin-left: 40px;
 		box-shadow: 5px 5px 10px #00000079;
 	}
+
 	.container {
-		margin-left: auto;
-		margin-right: auto;
-		overflow: auto;
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-end;
-		align-items: center;
-		width: fit-content;
-		background: rgba(250, 235, 215, 0.553);
-		padding: 7px;
-		border: 1px solid rgba(0, 0, 0, 0.1);
-		border-radius: 10px;
+		margin-top: 10px;
 	}
 	.button {
-		display: inline-block;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
 		font-size: 18px;
 		line-height: 48px;
 		height: 48px;
 		color: #ffffff;
 		border-radius: 10px;
+		box-shadow: none;
 		min-width: 200px;
 		background-color: #0b4972;
 		text-align: center;
-		transition: ease-in-out 0.1s;
+		transition: ease-in-out 0.3s;
+		outline: 0px;
+		border: none;
 		&:hover {
 			background-color: #0f0f9383;
 		}
+		&:active {
+			background-color: rgb(27, 130, 189);
+		}
 	}
+
 	.paragraphText {
 		margin-left: auto;
 		margin-right: auto;
+		margin-top: 20px;
 		max-width: 80%;
 		background-color: rgba(250, 235, 215, 0.513);
 		padding: 10px;
@@ -158,6 +160,6 @@
 		margin-right: auto;
 		max-height: 20em;
 		border-radius: 10px;
-		box-shadow: 1px 1px 2em #00000079;
+		box-shadow: 10px 10px 1em #00000079;
 	}
 </style>
